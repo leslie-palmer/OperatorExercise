625d501a76db12af44b06c8948a6ca42419a2cb4
leslie-palmer <scoopicus@gmail.com>
2022-05-26 04:10:13 -0700

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} + {b} = {a + b}");
+            Console.WriteLine($"{a} - {b} = {a - b}");
+            Console.WriteLine($"{a} x {b} = {a * b}");
+            Console.WriteLine($"{a} / {b} = {quotient} (remainder of {remainder})");
+
+            Console.WriteLine("We are going to find the area of a circle. Enter a number for the radius for our circle.");
+            var radius = double.Parse(Console.ReadLine());
+            var area = AreaOfCircle(radius);
+            Console.WriteLine($"A circle with a radius of {radius} units has an area of {area} square units!");
+        }
+    
+        public static double AreaOfCircle(double r)
+        {
+            return Math.PI * r * r;
+        }
+    
+    
+    }
+}
